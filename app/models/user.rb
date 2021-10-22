f344184b7c877434361c59718d7dffd212094c3b
murataholic <murataholic@gmail.com>
2021-10-22 21:19:37 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ヶー－〜・]+\z/ }
+
 end
