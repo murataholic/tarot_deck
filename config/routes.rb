0fe75f275657ce648977b28da60195fe66e542fb
murataholic <murataholic@gmail.com>
2021-11-03 16:30:36 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles do
     collection do
-      get 'search'
+      get 'search', 'search_result'
     end
   end
 end
