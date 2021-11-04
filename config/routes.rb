81851b9877b66eb666590c88933eb8e5373f4d9f
murataholic <murataholic@gmail.com>
2021-11-05 02:30:04 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'nices/create'
-  get 'nices/destroy'
   devise_for :users
   root to: 'articles#index'
   resources :articles do
+    resource :nices, only: [:create, :destroy]
     collection do
       get 'search', 'search_result', 'my_page'
     end
-    resource :nices, only: [:create, :destroy]
   end
 end
