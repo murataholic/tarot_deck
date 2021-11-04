a613e39a681674c460b87fa03b06079cabae8779
murataholic <murataholic@gmail.com>
2021-11-04 17:43:21 +0900

    マイグレーションファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_123458) do
+ActiveRecord::Schema.define(version: 2021_11_04_061227) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 2021_10_22_123458) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "nices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_nices_on_article_id"
+    t.index ["user_id", "article_id"], name: "index_nices_on_user_id_and_article_id", unique: true
+    t.index ["user_id"], name: "index_nices_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -76,4 +86,6 @@ ActiveRecord::Schema.define(version: 2021_10_22_123458) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "nices", "articles"
+  add_foreign_key "nices", "users"
 end
