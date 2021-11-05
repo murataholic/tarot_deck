66fe81f0ea3c7338014b74598376e9190f9c7ac1
murataholic <murataholic@gmail.com>
2021-11-06 00:57:53 +0900

    ER図・画面遷移図・READMEの修正

--- db/migrate/20211104061227_create_nices.rb
+++ db/migrate/20211104061227_create_nices.rb
@@ -3,7 +3,6 @@ class CreateNices < ActiveRecord::Migration[6.0]
     create_table :nices do |t|
       t.references :user, null: false, foreign_key: true
       t.references :article, null: false, foreign_key: true
-
       t.timestamps
       t.index [:user_id, :article_id], unique: true
     end
